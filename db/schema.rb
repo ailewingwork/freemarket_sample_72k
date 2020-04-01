539ff9cd87873b8d3bc32df669adbd3aa15f73f0
w-tensyo <dr.tensyo@gmail.com>
2020-04-02 02:07:28 +0900

    create sign in display

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_025500) do
+ActiveRecord::Schema.define(version: 2020_04_01_104457) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2020_03_31_025500) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
+    t.string "family_name", null: false
+    t.string "first_name", null: false
+    t.string "family_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.date "birth_year"
+    t.date "birth_month"
+    t.date "birth_day"
+    t.string "mobile", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
