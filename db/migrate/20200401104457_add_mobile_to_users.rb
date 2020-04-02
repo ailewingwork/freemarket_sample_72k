f02307c4105b57410a9c6cff518aa7270f72246e
w-tensyo <dr.tensyo@gmail.com>
2020-04-02 13:23:06 +0900

    userテーブルの編集とProfileテーブルの作成

--- db/migrate/20200401104457_add_mobile_to_users.rb
+++ db/migrate/20200401104457_add_mobile_to_users.rb
@@ -1,13 +1,5 @@
 class AddMobileToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :nickname, :string, null: :false
-    add_column :users, :family_name, :string, null: false
-    add_column :users, :first_name, :string, null: false
-    add_column :users, :family_name_kana, :string, null: false
-    add_column :users, :first_name_kana, :string, null: false
-    add_column :users, :birth_year, :date, null: :false
-    add_column :users, :birth_month, :date, null: :false
-    add_column :users, :birth_day, :date, null: :false
-    add_column :users, :mobile, :string, null: false, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_104457) do
+ActiveRecord::Schema.define(version: 2020_04_02_040843) do
+
+  create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "profile"
+    t.string "prof_image"
+    t.string "fimily_name", null: false
+    t.string "first_name", null: false
+    t.string "fimily_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.date "birth_year", null: false
+    t.date "birth_month", null: false
+    t.date "birth_day", null: false
+    t.string "mobile", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -21,16 +38,9 @@ ActiveRecord::Schema.define(version: 2020_04_01_104457) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nickname"
-    t.string "family_name", null: false
-    t.string "first_name", null: false
-    t.string "family_name_kana", null: false
-    t.string "first_name_kana", null: false
-    t.date "birth_year"
-    t.date "birth_month"
-    t.date "birth_day"
-    t.string "mobile", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users"
 end
