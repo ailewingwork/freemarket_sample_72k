ab65d687d1955bba21a00728f605fc198f776f51
naoto-ysd <na_oto1111@yahoo.co.jp>
2020-04-03 14:59:13 +0900

    fix-deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ lock '3.12.1'
 set :application, 'freemarket_sample_72k'
 
 # どのリポジトリからアプリをpullするかを指定する
+set :branch, 'deproy'
 set :repo_url,  'git@github.com:ailewingwork/freemarket_sample_72k.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
