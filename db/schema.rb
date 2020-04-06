d530de52bd037039813e4ec7634ebf00163397d7
naoto-ysd <na_oto1111@yahoo.co.jp>
2020-04-06 18:02:49 +0900

    Create-Images-Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_02_042557) do
+ActiveRecord::Schema.define(version: 2020_04_06_084304) do
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "image", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_images_on_item_id"
+  end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "product_name", null: false
@@ -58,6 +66,7 @@ ActiveRecord::Schema.define(version: 2020_04_02_042557) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "images", "items"
   add_foreign_key "items", "users"
   add_foreign_key "items", "users", column: "seller_id"
   add_foreign_key "profiles", "users"
