bde95de3890ea89b43fd38dd65a4a6c952ca7089
w-tensyo <dr.tensyo@gmail.com>
2020-04-09 12:49:34 +0900

    fix
    不要なファイルの削除と、リストのコンテンツ修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 
   def show
     # @user = User.find(params[:id])
-    @contents = [{name: "クレジットカード登録", path:"#", pattern: "GET",list_id: "credit_card"},{name: "ログアウト", path: "/users/sign_out", pattern: "DELETE",list_id: "logout"}]
+    @contents = [{name: "TOPへ戻る", path: "/", pattern: "GET",list_id: "to_top"},{name: "クレジットカード登録", path:"#", pattern: "GET",list_id: "credit_card"},{name: "ログアウト", path: "/users/sign_out", pattern: "DELETE",list_id: "logout"}]
   end
 
   def logout
