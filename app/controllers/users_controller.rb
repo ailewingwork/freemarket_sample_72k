1005a466ae6890d2af4a7219bd0a8dd9f18dce8f
w-tensyo <dr.tensyo@gmail.com>
2020-04-09 12:40:05 +0900

    create mypage display
    マイページ画面のフロントサイドの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,19 @@
 class UsersController < ApplicationController
 
   def index
-    
   end
 
   def new_1st_view
-    
   end
 
+  def show
+    # @user = User.find(params[:id])
+    @contents = [{name: "クレジットカード登録", path:"#", pattern: "GET",list_id: "credit_card"},{name: "ログアウト", path: "/users/sign_out", pattern: "DELETE",list_id: "logout"}]
+  end
+
+  def logout
+    binding.pry
+  end
 end
+
+
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,8 @@ class Item < ApplicationRecord
   has_many :images, dependent: :destroy
   has_many :comments, dependent: :destroy
   belongs_to :user
-  belongs_to :buyer, class_name:'User', :foreign_key => "buyer_id"
-  belongs_to :seller, class_name:'User', :foreign_key => "seller_id"
+  belongs_to :buyer, class_name:'User', foreign_key: "buyer_id"
+  belongs_to :seller, class_name:'User', foreign_key: "seller_id"
   belongs_to :brand
   belongs_to :category
   
