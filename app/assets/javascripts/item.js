10549d5b899dcad5c8c0bb8ed311ad46f5803cdc
w-tensyo <dr.tensyo@gmail.com>
2020-04-13 18:06:02 +0900

    一旦画像一枚で投稿できるように実装。

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -1,34 +1,34 @@
-$(function(){
-  $('.Upload').on('click', function(e){
-    console.log("aaa")
-    e.preventDefault();
-    let url = $(this).attr('action')
+// $(function(){
+//   $('.Upload').on('click', function(e){
+//     console.log("aaa")
+//     e.preventDefault();
+//     let url = $(this).attr('action')
 
-    // // messages#createにいく
-    // $.ajax({
-    //   url: url,
-    //   type: "POST",
-    //   data: formData,
-    //   dataType: 'json',
-    //   processData: false,
-    //   contentType: false
-    // })
-    .done(function(){
-      console.log("bbb")
-    })
-    .fail(function() {
-      alert("画像のアップロードに失敗しました。");
-    })
-  })
+//     // // messages#createにいく
+//     // $.ajax({
+//     //   url: url,
+//     //   type: "POST",
+//     //   data: formData,
+//     //   dataType: 'json',
+//     //   processData: false,
+//     //   contentType: false
+//     // })
+//     .done(function(){
+//       console.log("bbb")
+//     })
+//     .fail(function() {
+//       alert("画像のアップロードに失敗しました。");
+//     })
+//   })
 
-  // $(document).on("click", ".chat-group-user__btn--add", function() {
-  //   const userName = $(this).attr("data-user-name");
-  //   const userId = $(this).attr("data-user-id");
-  //   $(this)
-  //     .parent()
-  //     .remove();
-  //   addDeleteUser(userName, userId);
-  //   addMember(userId);
-  // });
+//   // $(document).on("click", ".chat-group-user__btn--add", function() {
+//   //   const userName = $(this).attr("data-user-name");
+//   //   const userId = $(this).attr("data-user-id");
+//   //   $(this)
+//   //     .parent()
+//   //     .remove();
+//   //   addDeleteUser(userName, userId);
+//   //   addMember(userId);
+//   // });
   
-})
+// })
